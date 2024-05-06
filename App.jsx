f1c996ff74c56264de76866a8eec221961ea047f
João Paulo Marcuz <126777006+Marcuz015@users.noreply.github.com>
2024-05-06 14:36:21 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,36 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList, ScrollView, TextInput } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import Empresa from './empresa';
 
-const dados = [
-  {id: 1, nome: 'x', valorMercado: '150'},
-  {id: 2, nome: 'a', valorMercado: '250'},
-  {id: 3, nome: 'c', valorMercado: '350'},
-  {id: 4, nome: 'x', valorMercado: '150'},
-  {id: 5, nome: 'a', valorMercado: '250'},
-  {id: 6, nome: 'c', valorMercado: '350'},
-  {id: 7, nome: 'x', valorMercado: '150'},
-  {id: 8, nome: 'a', valorMercado: '250'},
-  {id: 9, nome: 'c', valorMercado: '350'},
-]
-
 export default function App() {
+  
+const [dados, setEmpresas] = useState([]);
+
+async function getDados(url = ''){
+  const response = await fetch(url,{
+    method: "GET",
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers:{
+      "content-Type":"apllication/json"
+    }
+  });
+  return response.json()
+}
+
+useEffect(() => {
+  getDados('http://localhost:3000/empresa').then((listaDeEmpresa) =>{
+    console.log(listaDeEmpresa);
+    setEmpresas(listaDeEmpresa);
+  });
+},[])
+
   return (
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Dados empresas</Text>
+      <Text style={styles.titulo}>Dados empresas</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
       <View style={styles.lista}>
         <FlatList
         data={dados}
         renderItem={({item}) =>{
           return(
-            <Empresa nome={item.nome}
-            valorMercado={item.valorMercado}
+            <Empresa 
+              nome={item.Nome}
+              valorMercado={item.ValorDeMercado}
             />
           )
         }}
         />
       </View>
     </View>
+    </ScrollView>
   );
 }
 
@@ -43,5 +63,16 @@ const styles = StyleSheet.create({
   },
   lista:{
     width: 400
+  },
+  titulo:{
+    fontSize: 25,
+    marginBottom: 30,
+    marginTop: 50
+  },
+  input:{
+    borderWidth: 1,
+    width: 200,
+    textAlign: 'center',
+    borderRadius: 20
   }
 });
